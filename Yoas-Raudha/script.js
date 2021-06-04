d3aa1f12866808cdd7203149ca1d8b67b4d16b95
Hafid-weddinginvitation <84441224+Hafid-weddingivitation@users.noreply.github.com>
2021-06-04 08:31:41 +0700

    Update script.js

--- Yoas-Raudha/script.js
+++ Yoas-Raudha/script.js
@@ -203,7 +203,7 @@ $(document).ready(function () {
 
 
     // Set the date we're counting down to
-    var countDownDate = new Date("Feb 21, 2021 08:00:00").getTime();
+    var countDownDate = new Date("Jun 19, 2021 10:00:00").getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
